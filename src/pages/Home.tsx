cdb3c597bfca987c2e924566e868ad9c78695ff7
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-04 22:20:45 +0100

    footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,48 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage,IonToolbar,IonTitle,
+  IonButtons,
+  IonBackButton,
+  IonButton,
+  IonIcon,
+  IonMenuButton,
+  IonSearchbar,
+  IonSegment,
+  IonFooter,
+  IonSegmentButton, } from '@ionic/react';
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
+   
     <IonPage>
       <IonHeader>
         <IonToolbar>
+        <IonButtons >
+        <IonButton>
+          <IonIcon slot="icon-only" name="star" />
+        </IonButton>
+      </IonButtons>
           <IonTitle>Ionic Blank</IonTitle>
         </IonToolbar>
+        
       </IonHeader>
       <IonContent className="ion-padding">
+        
         The world is your oyster.
         <p>
           If you get lost, the{' '}
           <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
             docs
           </a>{' '}
-          will be your guide.
         </p>
+        
       </IonContent>
+
+      <IonFooter>
+      <IonToolbar>
+        <IonTitle>Footer</IonTitle>
+      </IonToolbar>
+    </IonFooter>
+
     </IonPage>
   );
 };
