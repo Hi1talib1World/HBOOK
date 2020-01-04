3e8d6a9e34799f7c575b4d3d799c3b784c0b574e
Hi1talib1World <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-04 23:04:18 +0100

    add about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,20 @@
+import { IonContent, IonHeader, IonPage,IonToolbar,IonTitle,
+    IonButtons,
+    IonBackButton,
+    IonButton,
+    IonIcon,
+    IonMenuButton,
+    IonSearchbar,
+    IonSegment,
+    IonFooter, IonTabBar, IonTabButton, IonLabel, IonBadge } from '@ionic/react';
+  import React from 'react';
+
+  const About: React.FC = () => {
+    return (
+        <IonPage>
+            
+        </IonPage>
+
+        );
+    };
+    export default About;     
