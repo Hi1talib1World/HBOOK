f51159fc991327f63c47e6cc5039f5fdc0781c60
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-28 00:42:04 +0100

    up home tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,14 @@
-import { IonSegment, IonSegmentButton, IonLabel,IonContent, IonHeader, IonTabs, IonTabBar, IonTabButton, IonBadge , IonPage,IonPopover, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
+import { IonSegment,IonMenu,IonItem,IonList,IonRouterOutlet, IonSegmentButton, IonLabel,IonContent, IonHeader, IonTabs, IonTabBar, IonTabButton, IonBadge , IonPage,IonPopover, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonFabList } from '@ionic/react';
 import React , { useState } from 'react';
 
 const Home: React.FC = () => {
   const [showPopover, setShowPopover] = useState(false); 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+
+    
       <IonContent className="ion-padding">
-        
+            
         
           <IonPopover
           isOpen={showPopover}
@@ -27,10 +24,37 @@ const Home: React.FC = () => {
     </IonFab>
 
 
-    
+    <IonTabs>
+    <IonTabBar slot="bottom">
+      <IonTabButton tab="schedule">
+        <IonIcon name="calendar" />
+        <IonLabel>Schedule</IonLabel>
+        <IonBadge>6</IonBadge>
+      </IonTabButton>
+
+      <IonTabButton tab="speakers">
+        <IonIcon name="contacts" />
+        <IonLabel>Speakers</IonLabel>
+      </IonTabButton>
+
+      <IonTabButton tab="map">
+        <IonIcon name="map" />
+        <IonLabel>Map</IonLabel>
+      </IonTabButton>
+
+      <IonTabButton tab="about">
+        <IonIcon name="information-circle" />
+        <IonLabel>About</IonLabel>
+      </IonTabButton>
+    </IonTabBar>
+    <IonRouterOutlet></IonRouterOutlet>
+
+  </IonTabs>
+
       </IonContent>
      
     </IonPage>
+    
   );
 };
 
