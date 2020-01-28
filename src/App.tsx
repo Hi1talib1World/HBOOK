80798c8d546027c3e3efacfd300c02e63a2ab3c1
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-28 01:45:17 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+import TabRoot from './pages/TabRoot';
 
 /* Theme variables */
 import './theme/variables.css';
