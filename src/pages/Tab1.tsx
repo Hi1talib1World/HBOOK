9b004605110b8ba3016ee0f7805531cf6d44c530
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-29 21:29:46 +0100

    Update Tab1.tsx

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -20,17 +20,7 @@ const Tab1: React.SFC<any> = (props) => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-      <IonCard>
-      <IonCardHeader>
-        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-        <IonCardTitle>Card Title</IonCardTitle>
-      </IonCardHeader>
-
-      <IonCardContent>
-        Keep close to Nature's heart... and break clear away, once in awhile,
-        and climb a mountain or spend a week in the woods. Wash your spirit clean.
-      </IonCardContent>
-    </IonCard>
+      
         
       <IonPopover
           isOpen={showPopover}
