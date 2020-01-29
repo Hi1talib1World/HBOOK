c452b26c4c1dae13518dc33589039db2e6a1cce2
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-01-29 21:51:11 +0100

    Update Tab1.tsx

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,6 +2,9 @@ import {
   IonHeader,
   IonToolbar,
   IonTitle,
+ 
+  IonListHeader,
+  
   IonContent,IonSegment,IonMenu,IonPopover, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonItem,IonList,IonRouterOutlet, IonSegmentButton, IonLabel, IonFab, IonFabButton, IonIcon, IonFabList,
   IonButton
 } from "@ionic/react";
@@ -14,27 +17,48 @@ const Tab1: React.SFC<any> = (props) => {
 
   return (
     <>
+      <React.Fragment>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab One</IonTitle>
-        </IonToolbar>
+        
       </IonHeader>
       <IonContent>
-      
-        
-      <IonPopover
-          isOpen={showPopover}
-          onDidDismiss={e => setShowPopover(false)}
-        >
-          <p>This is popover content</p>
-        </IonPopover>
+        <IonCard class="welcome-card">
+          <img src="/assets/shapes.svg" alt=""/>
+          <IonCardHeader>
+            <IonCardSubtitle>Get Started</IonCardSubtitle>
+            <IonCardTitle>Welcome to Ionic</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              Now that your app has been created, you'll want to start building out features and
+              components. Check out some of the resources below for next steps.
+            </p>
+          </IonCardContent>
+        </IonCard>
 
-        <IonFab vertical="bottom" horizontal="end" slot="fixed">
-      <IonFabButton onClick={() => setShowPopover(true)}>
-        <IonIcon name="arrow-dropleft" />
-      </IonFabButton>
-    </IonFab>
+        <IonList lines="none">
+          <IonListHeader>
+            <IonLabel>Resources</IonLabel>
+          </IonListHeader>
+          <IonItem href="https://ionicframework.com/docs/" target="_blank">
+            <IonIcon slot="start" color="medium"  />
+            <IonLabel>Ionic Documentation</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/building/scaffolding" target="_blank">
+            <IonIcon slot="start" color="medium"  />
+            <IonLabel>Scaffold Out Your App</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/layout/structure" target="_blank">
+            <IonIcon slot="start" color="medium"  />
+            <IonLabel>Change Your App Layout</IonLabel>
+          </IonItem>
+          <IonItem href="https://ionicframework.com/docs/theming/basics" target="_blank">
+            <IonIcon slot="start" color="medium"  />
+            <IonLabel>Theme Your App</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
+    </React.Fragment>
     </>
   );
 };
